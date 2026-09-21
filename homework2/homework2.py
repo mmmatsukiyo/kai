210ee4f3ceafb11867ae1f52914a616a4d7cf5c2
Kai Matsumoto <kmatsu@berkeley.edu>
2026-09-21 00:55:17 -0700

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,114 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+'''
+Git is a Version Control System that will track changes to your code, as well as keeping a history of it to go back to
+GitHub is a server for Git, storing all these changes and the history of your code
+Git Bash allows Windows users to type commands as if they were using a Mac or Linux terminal
+'''
+
+# 2) What’s the difference between the terminal and the command line?
+'''
+The terminal is the window where the command line is and the command line is where things are typed in the terminal
+'''
+
+# 3) How does Windows PowerShell differ from Git Bash?
+'''
+Windows Powershell is the shell used for Windows
+Git Bash is a way for Windows users to use Mac/Linux-like commands to Windows
+'''
+
+# 4) What’s the difference between Anaconda, conda, and Python?
+'''
+Python is a coding language
+Conda manages packages for Python
+Anaconda is the distributor of the packages
+'''
+
+# 5) What is VS Code? 
+'''
+VS Code is a code editor that can help write and debug code in multiple different languages
+'''
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+'''
+Jupyter Notebook is a open-source web app that can share code, documents, etc
+'''
+
+# 7) What does ~/ mean?
+'''
+~/ represents the path to the home directory
+'''
+
+# 8) What’s the difference between an absolute path and a relative path?
+'''
+An absolute path starts at the root directory and goes all the way to where you want it to go and can work wherever
+A relative path is a short-cut path that can only go up a folder from where you are
+'''
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+'''
+Absolute: /Users/kaimatsumoto/python_decal_fa26/course_assignments/homework2
+Relative: ../course_assignments/homework2
+'''
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+'''
+Absolute: cd course_assignments
+Relative: cd ..
+'''
+
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+'''
+It would remove the current directory you're in
+'''
+
+# 12) What do the following commands do?
+'''
+git add: Adds your files to the staging area
+git commit: Commits your files to the version history
+git push: Pushes your commits to GitHub (the internet)
+'''
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+'''
+"git add ." adds all the files to the staging area
+"git add <file>" adds a specific file to the staging area
+'''
+
+# 14) What do "git status" and "git log -1" do?
+'''
+"git status" gives a report on what Git sees in the repository
+"git log -1" gives a report on the most recent commit done
+'''
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+'''
+Cloning a repository (git clone) creates a brand-new copy of a remote repository
+Pulling a repository (git pull) updates an already existing repository with new changes from a remote server
+'''
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+'''
+Using a NoneType variable properly. I troubleshooted it by doing some research on how it works
+'''
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+'''
+How will GitHub help us later on in class? Is it just easier to organize things?
+Is VS Code where we will do most of our code, or will we use GitHub itself at some point?
+'''
+
+# 18) Tell me a fun fact!
+'''
+Our galaxy is due for a supernova, but we haven't directly observed one since 1604 (Kepler's Supernova) nor detected one (G1.9+0.3)
+'''
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print(9+10) #Prints out 19 (though it should print out 21)
